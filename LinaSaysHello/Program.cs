061b76653e9efcc8b488a12ed943b39dd1a3c4b7
Linases <lina.tauraite@gmail.com>
2023-07-21 11:31:04 +0300

    initial commit

---
+++ LinaSaysHello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinaSaysHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! What’s your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Nice to meet you " + name);
+            Console.ReadLine();
+        }
+    }
+} 
