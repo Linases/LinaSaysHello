58613411498715899b16be97366deea3ac047101
Linases <lina.tauraite@gmail.com>
2023-07-26 13:39:09 +0300

    Task 3_2 started

--- LinaSaysHello/Program.cs
+++ LinaSaysHello/Program.cs
@@ -13,15 +13,33 @@ namespace LinaSaysHello
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hi! What’s your name?");
-            //string name = Console.ReadLine();
-            //Console.WriteLine("Nice to meet you " + name);
-            //Console.WriteLine("Anastasiya");
-            //Console.WriteLine("Vention");
-            //Console.ReadLine();
+            //Task 2//
+            Console.Write("Enter a random number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a == 0 || a == 15)
+            {
+                Console.WriteLine("Partly working!");
+            }
+            else if (a != 37)
+            {
+                Console.WriteLine("Working!");
+            }
+            else if (a < 50 || a >= 32)
+            {
+                Console.WriteLine("Working!");
+            }
+            else
+            {
+                Console.WriteLine("Does’t work");
+            }
 
-           
+            //Task 3//
+            Console.Write("Enter a random number: ");
+            int randomNumber1 = Convert.ToInt32(Console.ReadLine());
+            String result1 = (randomNumber1 % 2 == 0) ? "even number" : "odd number";
+            Console.WriteLine("{0} is {1}", randomNumber1, result1);
 
+            Console.ReadLine();
 
         }
     }
