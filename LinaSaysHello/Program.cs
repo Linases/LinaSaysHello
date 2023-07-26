b9eeaf10cea590e8b5250cc32e3d1224044b547b
Linases <lina.tauraite@gmail.com>
2023-07-26 13:11:50 +0300

    task3_1 completed

--- LinaSaysHello/Program.cs
+++ LinaSaysHello/Program.cs
@@ -13,16 +13,59 @@ namespace LinaSaysHello
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hi! What’s your name?");
-            //string name = Console.ReadLine();
-            //Console.WriteLine("Nice to meet you " + name);
-            //Console.WriteLine("Anastasiya");
-            //Console.WriteLine("Vention");
-            //Console.ReadLine();
+            //Task1//
+            int num = 20;
+            num += 5;
 
-           
+            Console.WriteLine("Variable:" + num);
 
+            //Task2//
 
+            Console.Write("Enter a 4 digit number of days: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int years = number / 365;
+            int months = (number - years * 365) / 30;
+            int days = (number - years * 365) - months * 30;
+
+            Console.WriteLine("This number represents: " + years + " years " + months + " months " + days + " days");
+
+            //Task3//
+            
+            Console.Write("Enter a random number: ");
+            int randomNumber = Convert.ToInt32(Console.ReadLine());
+            int result = (randomNumber + randomNumber * 2);
+            Console.WriteLine("{0} + {1}*2 = {2}", randomNumber, randomNumber, result);
+
+            // Task 4//
+            /*
+            //every variable can be named by var type
+            sbyte num1 = -34; //small number
+            int num2 = 4; // integer type number
+            string word = "Hello";// word
+            char chacacter = 'R'; // one symbol
+            decimal num3 = 23.093433222m;// most precise;
+            int num4 = 40000;
+            bool statement = true;// represents statement
+            int num5 = 0; //integer type number
+            */
+
+            //Task 5//
+            Console.Write("Enter a random number: ");
+            int randomNumber1 = Convert.ToInt32(Console.ReadLine());
+            String result1 = (randomNumber1 % 2 == 0) ? "even number" : "odd number";
+            Console.WriteLine("{0} is {1}", randomNumber1, result1);
+
+            /*
+            if (randomNumber1 % 2 == 0)
+            {
+                Console.WriteLine(randomNumber1 + " is an even number");
+            }
+            else
+            {
+                Console.WriteLine(randomNumber1 + " is an odd number ");
+            }
+            */
+            Console.ReadLine();
         }
     }
 }
