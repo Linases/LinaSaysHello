6f9c048d5aaec45f709f621c9ae4defb795ccfc3
Linases <lina.tauraite@gmail.com>
2023-07-26 11:37:37 +0300

    Merge Task - homework

--- LinaSaysHello/Program.cs
+++ LinaSaysHello/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +13,16 @@ namespace LinaSaysHello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi! What’s your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Nice to meet you " + name);
-            Console.WriteLine("Anastasiya");
-            Console.WriteLine("Vention");
-            Console.ReadLine();
+            //Console.WriteLine("Hi! What’s your name?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Nice to meet you " + name);
+            //Console.WriteLine("Anastasiya");
+            //Console.WriteLine("Vention");
+            //Console.ReadLine();
+
+           
+
+
         }
     }
-} 
+}
