992ed63036e5c950a5831700f89bb40531f9283b
Linases <lina.tauraite@gmail.com>
2023-07-27 14:19:37 +0300

    Tasks started 1-2

--- LinaSaysHello/Program.cs
+++ LinaSaysHello/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO.Ports;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LinaSaysHello
@@ -13,16 +16,39 @@ namespace LinaSaysHello
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hi! What’s your name?");
-            //string name = Console.ReadLine();
-            //Console.WriteLine("Nice to meet you " + name);
-            //Console.WriteLine("Anastasiya");
-            //Console.WriteLine("Vention");
-            //Console.ReadLine();
+            ////Task 1//
+            Console.Write("Enter any positive number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                sum += i;
 
-           
+            }
+            Console.WriteLine("Result: " + sum);
 
 
+            //Task2//
+            // Using a while loop write the multiplication table for the number 3 to the console.
+
+
+            int number_3 = 3;
+            int counter = 0;
+
+            while (counter <= 10)
+            {
+                Console.WriteLine("3 x " + counter + " = " + (counter * number_3));
+                counter++;
+            }
         }
+
     }
+
 }
+
+
+
+
+
+
+
