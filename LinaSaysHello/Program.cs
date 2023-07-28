54e092b30d9e23077a2a33ad60d88e1b05e4d7c0
Linases <lina.tauraite@gmail.com>
2023-07-28 16:27:18 +0300

    New commit

--- LinaSaysHello/Program.cs
+++ LinaSaysHello/Program.cs
@@ -10,6 +10,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LinaSaysHello
 {
@@ -43,8 +44,13 @@ namespace LinaSaysHello
             //Task 3//
             //Create an array of numbers[3, 5, 9, 8, 15].Find the product of these numbers and write the result to the console.
 
-            int[] numArray = { 3, 5, 9, 8, 15 };
-
+            //int[] numArray = { 3, 5, 9, 8, 15 };
+            //int value = 1;
+            //for (int i = 0; i < numArray.Length; i++)
+            //{
+            //    value = value * numArray[i];
+            //    Console.WriteLine(value);
+            //}
 
             //Task 4//
             //Write a program that calculates how many times 2048 must be divided by 2 to make it less than 10.
@@ -63,32 +69,94 @@ namespace LinaSaysHello
             //Task 5//
 
             //Create an array of strings.Write a program that analyzes a created array and, if there is a string with the value “Hello”, displays the word “Labas!” in console and leaves the loop once it is found.
-             string[] words = { "Hello", "World" };
-            for (int i = 0; i < words.Length; i++)
+            // string[] words = { "Hello", "World" };
+            //for (int i = 0; i < words.Length; i++)
+            //{
+
+            //}
+
+            //Task 6//
+            //Create an array of numbers.Write a program that calculates the sum
+            //of the first and last element of an array and writes it to the console
+
+
+            //int[] arrNumbers = { 4, 6, 8, 9, 10, 11 };
+            //for (int i = 0; i < arrNumbers.Length; i++)
+            //{
+            //    Console.Write(arrNumbers[i] + " ");
+            //}
+            //Console.WriteLine("\nSum of first and last element is: " + (arrNumbers[0] += arrNumbers[arrNumbers.Length - 1]));
+
+            //Task7//????
+            // Create an array of numbers.Find the sum of indexes of minimum and maximum array elements.
+            //    int[] arrNumbers1 = { 4, -6, 8, 900, 20, 11 };
+
+            //    for (int i = 0; i < arrNumbers1.Length; i++)
+            //    {
+            //        Console.WriteLine(arrNumbers1[i]);
+            //    }
+            //    int max = arrNumbers1[0];
+            //    int min = arrNumbers1[0];
+            //    for (int i = 0; i <= arrNumbers1.Length - 1; i++)
+            //    {
+            //        if (arrNumbers1[i] > max)
+            //        {
+            //            max = arrNumbers1[i];
+            //        }
+            //        if (arrNumbers1[i] < min)
+            //        {
+            //            min = arrNumbers1[i];
+            //        }
+            //    }
+            //    Console.WriteLine("The sum of indexes of minimum and maximum array elements is: " + (min += max));
+            //}
+            //Task8//
+            //Create an array of numbers and sort its elements in ascending order(without using function Sort).
+            int[] arrNumbers2 = { 4, -6, 8, 900, 20, 11 };
+            foreach (int value1 in arrNumbers2)
             {
-               
-                {
-
-                }
-
-    
+                Console.Write(value1 + " ");
             }
-                ////Task 9//
-            ////Write to the console a multiplication table for numbers from 1 to 10.
+            int temporar;
 
-            for (int i = 1; i <= 10; i++)
+            // traverse 0 to array length
+            for (int i = 0; i < arrNumbers2.Length - 1; i++)
+
+                // traverse i+1 to array length
+                for (int j = i + 1; j < arrNumbers2.Length; j++)
+
+                    // compare array element with
+                    // all next element
+                    if (arrNumbers2[i] > arrNumbers2[j])
+                    {
+
+                        temporar = arrNumbers2[i];
+                        arrNumbers2[i] = arrNumbers2[j];
+                        arrNumbers2[j] = temporar;
+                    }
+            Console.WriteLine("\nNumbers in asccending order:");
+            // print all element of array
+            foreach (int value2 in arrNumbers2)
             {
-                for (int j = 1; j <= 10; j++)
-                {
-                    Console.WriteLine(i + " * " + j + " = " + (i * j));
-                }
+                Console.Write(value2 + " ");
             }
+            //    ////Task 9//
+            //////Write to the console a multiplication table for numbers from 1 to 10.
 
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for (int j = 1; j <= 10; j++)
+            //    {
+            //        Console.WriteLine(i + " * " + j + " = " + (i * j));
         }
-
     }
-
 }
+
+
+
+
+
+
 
 
 
