59e26053e0e29185784c610afaf15b39f279d5d7
Linases <lina.tauraite@gmail.com>
2023-07-28 12:02:45 +0300

    Task 4 tasks doing

--- LinaSaysHello/Program.cs
+++ LinaSaysHello/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.IO.Ports;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Reflection.Emit;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
@@ -16,30 +17,73 @@ namespace LinaSaysHello
     {
         static void Main(string[] args)
         {
-            ////Task 1//
-            Console.Write("Enter any positive number: ");
-            int number = Convert.ToInt32(Console.ReadLine());
-            int sum = 0;
-            for (int i = 1; i <= number; i++)
+            //////Task 1//
+            //Console.Write("Enter any positive number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    sum += i;
+
+            //}
+            //Console.WriteLine("Result: " + sum);
+
+
+            ////Task2//
+
+            //int number_3 = 3;
+            //int counter = 0;
+
+            //while (counter <= 10)
+            //{
+            //    Console.WriteLine("3 x " + counter + " = " + (counter * number_3));
+            //    counter++;
+            //}
+
+            //Task 3//
+            //Create an array of numbers[3, 5, 9, 8, 15].Find the product of these numbers and write the result to the console.
+
+            int[] numArray = { 3, 5, 9, 8, 15 };
+
+
+            //Task 4//
+            //Write a program that calculates how many times 2048 must be divided by 2 to make it less than 10.
+
+            //int number_2048 = 2048;
+            //int divisionTimes = 0;
+            //int result = number_2048 / 2;
+            //while (result > 10)
+            //{
+            //    Console.WriteLine("Number after division: " + result);
+            //    result /= 2;
+            //    divisionTimes++;
+            //}
+            //Console.WriteLine(" It took " + divisionTimes + " times to divite 2048 by 2 till it's less than 10");
+
+            //Task 5//
+
+            //Create an array of strings.Write a program that analyzes a created array and, if there is a string with the value “Hello”, displays the word “Labas!” in console and leaves the loop once it is found.
+             string[] words = { "Hello", "World" };
+            for (int i = 0; i < words.Length; i++)
             {
-                sum += i;
+               
+                {
 
+                }
+
+    
             }
-            Console.WriteLine("Result: " + sum);
+                ////Task 9//
+            ////Write to the console a multiplication table for numbers from 1 to 10.
 
-
-            //Task2//
-            // Using a while loop write the multiplication table for the number 3 to the console.
-
-
-            int number_3 = 3;
-            int counter = 0;
-
-            while (counter <= 10)
+            for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine("3 x " + counter + " = " + (counter * number_3));
-                counter++;
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine(i + " * " + j + " = " + (i * j));
+                }
             }
+
         }
 
     }
