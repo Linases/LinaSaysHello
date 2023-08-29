b1788fd6abc7fecd9d66a7370aba48b61bc36d51
Linases <lina.tauraite@gmail.com>
2023-08-29 23:28:44 +0300

    Last commit

--- ExceptionsHandling/ExceptionsHandling.cs
+++ ExceptionsHandling/ExceptionsHandling.cs
@@ -11,44 +11,41 @@ namespace AqaLabHomeworkFilesStorage.Task8
         public static void ShowArrayElement()
         {
             var array = new int[] { 8, 7, 1, 4, 2 };
-            Console.WriteLine("Input index of element in array:");
-            var inputedValue = Console.ReadLine();
-            try
+            while (true)
             {
-                var elementIndex = inputedValue.Equals(string.Empty) ? null : inputedValue;
-
-                if (int.Parse(elementIndex) < 0)
-                {
-                    throw new NegativeIndexException("Negative number is not allowed. Please enter a positive integer index.");
-                }
-
-                int parsedElementIndex = int.Parse(elementIndex);
-                int elementOfarrayByIndex = array[parsedElementIndex];
-                Console.WriteLine($"Array element that has index {inputedValue} has value {elementOfarrayByIndex}");
-
-            }
-            catch (IndexOutOfRangeException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine("Error!:" + ex.Message + "Please enter a valid integer index");
-            }
-            catch (ArgumentNullException ex)
-            {
-                Console.WriteLine("Error!: Input can not be empty. Please enter a valid integer index");
-            }
-            catch (NegativeIndexException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                Console.ReadLine();
                 Console.WriteLine("Input index of element in array:");
-            }
+                var inputedValue = Console.ReadLine();
+                try
+                {
+                    var elementIndex = inputedValue.Equals(string.Empty) ? null : inputedValue;
 
+                    if (int.Parse(elementIndex) < 0)
+                    {
+                        throw new NegativeIndexException("Negative number is not allowed. Please enter a positive integer index.");
+                    }
+
+                    int parsedElementIndex = int.Parse(elementIndex);
+                    int elementOfarrayByIndex = array[parsedElementIndex];
+                    Console.WriteLine($"Array element that has index {inputedValue} has value {elementOfarrayByIndex}");
+                    return;
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Error!:" + ex.Message + "Please enter a valid integer index");
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine("Error!: Input can not be empty. Please enter a valid integer index");
+                }
+                catch (NegativeIndexException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 
