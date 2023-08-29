55599e3f525f4e83035dd3db6b65141551636ea7
Linases <lina.tauraite@gmail.com>
2023-08-29 16:12:05 +0300

    Working on task

--- ExceptionsHandling/Program.cs
+++ ExceptionsHandling/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿using AqaLabHomeworkFilesStorage.Task8;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        ExceptionsHandling.ShowArrayElement();
+    }
+}
