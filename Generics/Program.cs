09b9fbd2c12223bc72f21b32ec112ed5973c7a33
Linases <lina.tauraite@gmail.com>
2023-08-29 23:12:18 +0300

    testing

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        GenericArray<string> array = new GenericArray<string>(3);
+        array.AddObject("one");
+        array.AddObject("two");
         
     }
 }
