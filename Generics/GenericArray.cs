5bc18ba128cadfbc75fdf1ee34ac771fbea719ae
Linases <lina.tauraite@gmail.com>
2023-08-29 22:58:46 +0300

    Task started

---
+++ Generics/GenericArray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class GenericArray<T>
+
+    {
+        private T[] array;
+        private int count;
+        public GenericArray(int length)
+        {
+            array = new T[length];
+            count = 0;
+        }
+
+        public void AddObject(T item)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == null)
+                {
+                    array[i] = item;
+                    count++;
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Not possible to add. Array is already full.");
+                }
+            }
+        }
+        public void RemoveObject<T>(ref T[] arr, int index)
+        {
+            for (int i = index; i < arr.Length - 1; i++)
+            {
+
+                arr[i] = arr[i + 1];
+            }
+            Array.Resize(ref arr, arr.Length - 1);
+        }
+
+
+        public T GetItemByIndex(int index)
+        {
+            {
+                return array[index];
+            }
+
+
+
+
+        }
+        public int CountObjects() => count;
+
+
+    }
+}
+
