49f8d29934f4dd413691291b4f918641ec0165e7
Linases <lina.tauraite@gmail.com>
2023-08-31 13:16:40 +0300

    initail commit

---
+++ Collections/List.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class List<T>
+    {
+        public List()
+        {
+
+        }
+
+        public static int SumOfEvenNumbers()
+        {
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0) // Check if the number is even
+                {
+                    sum += number; // Add the even number to the sum
+                }
+            }
+
+            return sum;
+        }
+
+
+
+
+    }
+}
