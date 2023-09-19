3aa3040e4be6af56549dd7e9bbad51795492e09c
Linases <lina.tauraite@gmail.com>
2023-09-19 15:42:10 +0300

    commit

--- NUnitTestProject/AssertionsTest.cs
+++ NUnitTestProject/AssertionsTest.cs
@@ -8,13 +8,32 @@ using System.Threading.Tasks;
 namespace NUnitTestProject
 {
     [TestFixture]
-    internal class AssertionsTest0 
+    internal class AssertionsTest0
     {
-        [TestCase (0,1,2)]
+        [TestCase(0, 1, 2)]
         public void FailTest(int num1, int num2, int expextedResult)
         {
-            Assert.Fail("failure",num1 / num2 ==expextedResult);
+            Assert.Fail("failure", num1 / num2 == expextedResult);
         }
-
+        [Test]
+        public void EmptyTest ()
+        {
+            string a = string.Empty;
+            Assert.That(a, Is.Empty);
+        }
+        [Test]
+        public void CollectionTest()
+        { 
+            List <int> intList = new List<int> () {1,2,3,4};
+            int variable = 1;
+            Assert.Contains(variable,intList);
+            Assert.That(intList, Is.All.Positive);
+            Assert.That(intList.Count, Is.Not.EqualTo(0));
+        }
+        [Test]
+        public void ExceptionTest()
+        {
+            Exception ex = new Exception();
+        }
     }
 }
